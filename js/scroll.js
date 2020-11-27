2ef4eaeb581403c21d1e95dca688f0c91c5e7ce4
seungyeol1226 <67949323+seungyeol1226@users.noreply.github.com>
2020-11-27 10:02:16 +0900

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,10 @@
 window.addEventListener('DOMContentLoaded', function () {
     var conEle = document.querySelector('article');
-    var divEle = document.querySelectorAll('article .page');
+    var pageEle = document.querySelectorAll('article .page');
     var conPos = [];
     var idx = 0;
     
-    divEle.forEach(function(el){
+    pageEle.forEach(function(el){
         conPos.push(el.offsetTop);
     });    
  
@@ -18,22 +18,55 @@ window.addEventListener('DOMContentLoaded', function () {
         
         if(e.wheelDelta < 0){
             //down
-            if(idx < divEle.length-1){idx++;}
+            if(idx < pageEle.length-1){idx++;}
         }else{
             //up
             if(idx > 0){idx--;}
         }
         
         conEle.style.transform = "translateY(-"+ conPos[idx]+"px)";
+        update();
         
         bln=true;
         setTimeout(function(){bln=false;},1000);
     });
-    // ---------------------------------------------------------------
+        var menuEl = document.querySelectorAll('.h-menu a');
+        var pageEl = document.querySelectorAll('.page');
+        var pageArr = [];
 
+        pageEl.forEach(function(sl,idx){
+            pageArr.push(sl.offsetTop);
+        });
+        var asideEl = document.querySelector('aside');
+            var asideTop = asideEl.offsetTop;
 
+        menuEl.forEach(function(sl,i){
+            sl.addEventListener('click',function(e){
+                idx = i;
+                e.preventDefault();
+                // window.scrollTo(0,pageArr[idx]);
+                conEle.style.transform = "translateY(-"+ pageArr[idx]+"px)";
+                update();
+            });
+            asideEl.addEventListener('click',function(){
+                conEle.style.transform =  "translateY(-"+ pageArr[0]+"px)";
+                pageArr.push('0')
+            })
+        });
 
-//    --------------------------------------------------------
+        var winH = window.innerHeight / 2;
+        function update(){
+            menuEl.forEach(function(a){
+                a.classList.remove('active');
+            });
+            menuEl[idx].classList.add('active')
+
+            
+        }
+        
+        
+            
+        
+            
 
 });
-
